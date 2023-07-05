16f586d5fd254f3fa96d400e2c195e68ba823dc3
yashmeet-coder <meetyash681@gmail.com>
2023-07-05 11:51:14 +0530

    updated styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,10 +16,10 @@ const Home = ({ socket }) => {
   return (
     <div>
       <form className='home__container' onSubmit={handleSubmit}>
-        <motion.h2 animate={{ x:0,opacity:1, scale:1 }} transition={{
-          duration: 3,
-          ease: [0.5, 0.71, 1, 1.5],
-        }} initial={{ opacity: 0, scale: 0.5 }}
+        <motion.h2 animate={{ x:[-35,0],opacity:1}} transition={{
+          duration: 1.5,
+          ease: [0.3, 0.71, 1, 1.5],
+        }} initial={{ opacity: 0}}
        className="home__header">Sign in to Open Chat</motion.h2>
         <label htmlFor="username">Username</label>
         <input
@@ -31,7 +31,7 @@ const Home = ({ socket }) => {
           value={userName}
           onChange={(e) => setUserName(e.target.value)}
         />
-        <button className="home__cta">SIGN IN</button>
+        <motion.button whileHover={{ scale: 1.2 }} className="home__cta">SIGN IN</motion.button>
       </form>
     </div>
   )
